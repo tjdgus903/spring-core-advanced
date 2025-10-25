cb1dc447adf1ee3434ecea51b83b0e8876d6dcc6
tjdgus903 <tjdgusgus123@gmail.com>
2025-10-25 10:00:55 +0900

    [V0] request 기능 추가

--- src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -3,22 +3,25 @@ package hello.advanced.app.v0;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import static java.lang.Thread.sleep;
+
+// 컴포넌트 스캔의 대상이 되는데 컴포넌트 스캔이랑 스프링 컨테이너가 자동으로 Bean을 찾아서 등록하는 기능
 @Repository
-@RequiredArgsConstructor
+@RequiredArgsConstructor    // 컴포넌트 스캔의 대상, 필요한 생성자를 자동으로 만들어주는 기능
 public class OrderRepositoryV0 {
 
-    public void save(String itemId) {
-        //저장 로직
-        if (itemId.equals("ex")) {
+    public void save(String itemId){
+        // 저장 로직
+        if(itemId.equals("ex")){
             throw new IllegalStateException("예외 발생!");
         }
         sleep(1000);
     }
 
-    private void sleep(int millis) {
-        try {
+    private void sleep(int millis){
+        try{
             Thread.sleep(millis);
-        } catch (InterruptedException e) {
+        }catch(InterruptedException e){
             e.printStackTrace();
         }
     }
--- src/main/java/hello/advanced/app/v0/OrderServiceV0.java
+++ src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -4,12 +4,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor    // 컴포넌트 스캔의 대상
 public class OrderServiceV0 {
-
     private final OrderRepositoryV0 orderRepository;
 
-    public void orderItem(String itemId) {
+/*
+    @RequiredArgsConstructor 는 자동으로 final, NonNull 필드의 자동생성자를 생성함
+    이 생성자가 없으면 아래 코드를 추가해줘야됨
+
+    @Autowired
+    public OrderService(OrderRepositoryV0 orderRepository){
+        this.orderRepository = orderRepository;
+    }
+*/
+
+    public void orderItem(String itemId){
         orderRepository.save(itemId);
     }
 }
